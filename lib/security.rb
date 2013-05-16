1e53d3fe57fb2740e70fe7e6374491c83685e52d
Anthony Carlos <anthony@carlosfamily.com>
2013-05-16 01:14:39 -0400

    Add security model.

--- lib/security.rb
+++ lib/security.rb
@@ -1,13 +1,8 @@
-require 'date'
-require 'bigdecimal'
-
 class Security
-  attr_reader :symbol, :description, :market_price, :market_date
-  def initialize(symbol, description, market_price, market_date)
+  attr_reader :symbol, :description
+  def initialize(symbol, description)
     @symbol = symbol
     @description = description
-    @market_price = BigDecimal.new(market_price)
-    @market_date = market_date
   end
 end
 
